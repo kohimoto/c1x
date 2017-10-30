1adecb257e013344973b159ae5819a855e488e06
kohimoto <kohimotoj@gmail.com>
2017-10-30 16:24:16 +0900

    animate

--- js/yuka.js
+++ js/yuka.js
@@ -1,7 +1,4 @@
 $(function(){
-
-
-		// Animations
 	$(window).scroll(function () {
 		if($(window).scrollTop() > 2400) {
 			$('#move_idsp1').animate({'left':'-200px'});
@@ -16,30 +13,4 @@ $(function(){
 			$('#move_e-commerce').animate({'right':'-100px'});
 		}
 	});
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				i++;
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated');
-							} else {
-								el.addClass('fadeInUp animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-				}, 100);
-			}
-		} , { offset: '85%' } );
-	};
-
-
 });
