353c3445daebd53d562ebd4eb951249044d5a05b
kohimoto1! <kohimotoj@gmail.com>
2017-10-31 16:52:16 +0900

    boreder animate

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(function(){
 	if($('.line_issp1').length){
 	var target_issp = $('.line_issp1').offset().top+200;
 	var windowHeight = $(window).height();
-	var flg = false;
+	var flg1 = false;
 	var scrollTop = 0;
 	var startPoint = 0;
 
@@ -52,7 +52,7 @@ $(function(){
 		}
 		}
 	});
-		var mySVG2 = $('.line_issp').drawsvg();
+		var mySVG2 = $('.line_issp2').drawsvg();
 	
 	$(window).on('scroll',function(){
 
@@ -60,9 +60,9 @@ $(function(){
 		windowHeight = $(window).height();
 		startPoint = scrollTop + windowHeight - 100;
 		if(startPoint > target_issp){
-		if(flg == false){
+		if(flg1 == false){
 		mySVG.drawsvg('animate');
-		flg = true;
+		flg1 = true;
 
 		}
 		}
@@ -75,38 +75,54 @@ $(function(){
 	//page idsp 
 
 	if($('.line_idsp1').length){
-	var target_idsp = $('.line_idsp1').offset().top+200;
+	var target_idsp1 = $('.line_idsp1').offset().top+200;
 	var target_idsp3 = $('.line_idsp3').offset().top+200;
-	var target_idsp4 = $('.line_idsp4').offset().top+200;
 	var windowHeight = $(window).height();
-	var flg = false;
-	var flg_stg2 = false;
-	var flg_stg2_2 = false;
+	var flg1 = false;
+	var flg2 = false;
+	var flg3 = false;
+	var flg4 = true;
+	var flg5 = true;
 	var scrollTop = 0;
 	var startPoint = 0;
 
-	var flg2 = false;
 
 	var mySVG = $('.line_idsp1').drawsvg({
 		duration: 1000,
 		stagger: 400,
 		easing: 'swing',
-		reverse: false,
-		callback: function(){
-		if(flg2 == false){
-		mySVG2.drawsvg('animate');
-		flg2 = true;
-		}
-		}
+		reverse: false
 	});
 	var mySVG2 = $('.line_idsp2').drawsvg();
 	var mySVG3 = $('.line_idsp3').drawsvg({
 		duration: 1000,
-		stagger: 5000,
+		stagger: 1000,
+		easing: 'swing',
+		reverse: false,
+		callback: function next_4(){
+		if(flg2 == false){
+		mySVG2.drawsvg('animate');
+
+		flg2 = true;
+		}else if(flg4 == false){
+		mySVG4.drawsvg('animate');
+		flg4 = true;
+		flg5 = false;
+
+		}else if(flg5 == false){
+		mySVG5.drawsvg('animate');
+		flg5 = true;
+		
+		}
+		}
+	});
+	var mySVG4 = $('.line_idsp4').drawsvg({
+		duration: 1000,
+		stagger: 1000,
 		easing: 'swing',
 		reverse: false
 	});
-	var mySVG4 = $('.line_idsp4').drawsvg({
+	var mySVG5 = $('.line_idsp5').drawsvg({
 		duration: 1000,
 		stagger: 1000,
 		easing: 'swing',
@@ -118,30 +134,20 @@ $(function(){
 		scrollTop = $(window).scrollTop();
 		windowHeight = $(window).height();
 		startPoint = scrollTop + windowHeight - 100;
-		if(startPoint > target_idsp){
-		if(flg == false){
+		if(startPoint > target_idsp1){
+		if(flg1 == false){
 		mySVG.drawsvg('animate');
-		flg = true;
+		flg1 = true;
 
 		}
 		}
 		if(startPoint > target_idsp3){
-		//Whats C1X iDSP
-		if(flg_stg2 == false){
+		if(flg3 == false){
+		flg4 = false; 
 		mySVG3.drawsvg('animate');
-		flg_stg2 = true;
+		flg3 = true;
 
 		}
-		}
-
-		if(startPoint > target_idsp4){
-		//Whats C1X iDSP
-		if(flg_stg2_2 == false){
-		mySVG4.drawsvg('animate');
-		flg_stg2_2 = true;
-
-		}
-
 		}
 
 
